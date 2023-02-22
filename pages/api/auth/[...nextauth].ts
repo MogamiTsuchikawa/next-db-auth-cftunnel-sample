2c4a244f1f08b9e83dad1f076eb151a2e15dbcd7
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2023-02-22 13:17:58 +0900

    sessionからuser idを取得できるように

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,9 +1,10 @@
-import NextAuth from "next-auth";
+import NextAuth, { Session, User } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "../../../lib/prismadb";
+import { AdapterUser } from "next-auth/adapters";
 
-export default NextAuth({
+export const authOption = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
@@ -11,4 +12,21 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
-});
+  callbacks: {
+    async redirect({ baseUrl }: { baseUrl: string }) {
+      return baseUrl;
+    },
+    async session({
+      session,
+      user,
+    }: {
+      session: Session;
+      user: User | AdapterUser;
+    }) {
+      if (session?.user) session.user.id = user.id;
+      return session;
+    },
+  },
+};
+
+export default NextAuth(authOption);
